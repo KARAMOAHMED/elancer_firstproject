698b1f170a16d70dfea61123d76733ee1e8a1829
KARAMOAHMED <91637936+KARAMOAHMED@users.noreply.github.com>
2021-11-08 09:29:02 +0200

    commit

---
+++ android/app/src/main/kotlin/com/example/elancer_firstproject/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.elancer_firstproject
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/launch_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class LaunchScreen extends StatefulWidget {
+  const LaunchScreen({Key? key}) : super(key: key);
+
+  @override
+  _LaunchScreenState createState() => _LaunchScreenState();
+}
+
+class _LaunchScreenState extends State<LaunchScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                colors: [
+                  Colors.blue.shade800,
+                  Colors.pink.shade800,
+                ]
+            )
+        ),
+        child: Text('Project1'),
+      ),
+    );
+  }
+}
